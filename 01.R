a87c09562738a85e38cc1a5593f9fa90368a0b73
Darwin <jan.bohm.93@gmail.com>
2020-12-02 17:49:44 +0100

    Add files via upload

---
+++ 01.R
@@ -0,0 +1,26 @@
+setwd("~/AoC/2020/01")
+
+input <- read.table("./input.txt")
+input <- as.numeric(unlist(c(input)))
+l <- length(input)
+
+#star 1
+for (i in 1:(l - 1)) {
+  for (j in (i + 1):(l)) {
+    if (input[i] + input[j] == 2020) {
+      print(input[i] * input[j])
+    }
+  }
+}
+
+#star 2
+
+for (i in (1:(l - 2))) {
+  for (j in ((i + 1):(l - 1))) {
+    for (k in ((j + 1):l)) {
+      if ((input[i] + input[j] + input[k]) == 2020) {
+        print(input[i] * input[j] * input[k])
+      }
+    }
+  }
+}
